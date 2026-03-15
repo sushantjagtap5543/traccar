3f9b943d93e133a573bad77b611b69779267092d
Sushant Jagtap <sushantjagtap5543@gmail.com>
2026-03-16 01:11:43 +0530

    Correct RolesGuard relative import path

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -2,8 +2,8 @@
 # Initialize backend
 
 # Apply environment variables
-sed -i "s|<TRACCAR_ADMIN_USER>|$TRACCAR_ADMIN_USER|g" traccar.xml
-sed -i "s|<TRACCAR_ADMIN_PASS>|$TRACCAR_ADMIN_PASS|g" traccar.xml
+sed -i "s|<TRACCAR_ADMIN_USER>|$TRACCAR_ADMIN_USER|g" conf/traccar.xml
+sed -i "s|<TRACCAR_ADMIN_PASS>|$TRACCAR_ADMIN_PASS|g" conf/traccar.xml
 
 # Start backend
-java -jar traccar-server.jar conf/traccar.xml
+java -jar tracker-server.jar conf/traccar.xml
--- services/api-server/src/common/guards/roles.guard.ts
+++ services/api-server/src/common/guards/roles.guard.ts
@@ -1,6 +1,6 @@
 import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
-import { UserRole } from '../database/entities/user-role.enum';
+import { UserRole } from '../../database/entities/user-role.enum';
 import { ROLES_KEY } from '../decorators/roles.decorator';
 
 @Injectable()
